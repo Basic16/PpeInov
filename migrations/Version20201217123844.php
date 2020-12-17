d7366ff7824833be0a1e517b832b117bc422a8ba
Idumi888 <vincent.dievart@epsi.fr>
2020-12-17 12:59:37 +0000

    profildebug

--- migrations/Version20201217123844.php
+++ migrations/Version20201217123844.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20201211101332 extends AbstractMigration
+final class Version20201217123844 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,6 +20,9 @@ final class Version20201211101332 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE entreprise (id INT AUTO_INCREMENT NOT NULL, numero_entreprise INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE fichier (id INT AUTO_INCREMENT NOT NULL, photo_profil VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE independant (id INT AUTO_INCREMENT NOT NULL, numero_independant INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE user ADD fichier_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D649F915CFE FOREIGN KEY (fichier_id) REFERENCES fichier (id)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649F915CFE ON user (fichier_id)');
@@ -29,6 +32,9 @@ final class Version20201211101332 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D649F915CFE');
+        $this->addSql('DROP TABLE entreprise');
+        $this->addSql('DROP TABLE fichier');
+        $this->addSql('DROP TABLE independant');
         $this->addSql('DROP INDEX UNIQ_8D93D649F915CFE ON user');
         $this->addSql('ALTER TABLE user DROP fichier_id');
     }
